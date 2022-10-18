31614ab892e8b1a7aae7713dc41f294034f73859
LwinMateo <lymateo-lopez@myseneca.ca>
2022-10-18 16:39:03 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+var moduleA = require('data-service.js');
+const { stringify } = require('querystring');
+
+
+var HTTP_PORT = process.env.HTTP_PORT || 8080;
+
+
+
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res){
+    let HTMLText = "<h2>Declaration:</h2>";
+
+    HTMLText += "<p>The rest text is displayed in paragraph as shown in screenshot.</p>";
+
+    HTMLText += "<p>I acknowledge the College's academic integrity policy - and my own integrity - remain in effect<br>"
+    HTMLText += "<p>whether my work is done remotely or onsite. Any test or assignment is an act of trust between<br>"
+    HTMLText += "<p>me and my instructor, and especially with my classmates.. even when no one is watching. I<br>"
+    HTMLText += "declare I will not break that trust.</p>"
+
+    HTMLText += "<p>Name: <span style='background-color:yellow'>Lwin Yonal Mateo Lopez</span></p>";
+    HTMLText += "<p>Student Number: <span style='background-color:yellow'>134710201</span></p>"
+
+    HTMLText += "<a href='/BSD'>Click to visit BSD Students</p>";
+    HTMLText += "<a href='/highGPA'>Click to see who has the highest GPA</p>"
+
+    res.send(HTMLText);
+});
+
+app.get('/*', function(req, res){
+    res.status(404).send("Error 404: page not found");
+})
+
+app.get('/BSD', function(req, res){
+    moduleA.getBSD().then((data) =>{
+        res.json(data);
+    })
+})
+
+app.get('/highGPA', function(req, res){
+    moduleA.highGPA().then((data) =>{
+        let text = "<h1>Highest GPA:</h1>";
+        text += "<p>Student ID: " + stringify(data.studID) + "<br>";
+        text += "<p>Name: " + data.name + "<br>";
+        text += "<p>Program: " + stringify(data.program) + "<br>";
+        text += "<p>GPA: " + stringify(data.gpa) + "</p>";
+
+        res.send(text);
+    })
+})
+
+moduleA.initialize().then(function(){
+    app.listen(HTTP_PORT, onHttpStart);
+}).catch(function(err){
+    console.log('Initialization Failed', err);
+})
---
+++ test2_moduleA.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const { resolve } = require('path');
+
+var students = new Array();
+
+module.exports.initialize = function(){
+    return new Promise(function(resolve, reject){
+        fs.readFile('/students.jsong', (err, data) => {
+            if(err){
+                reject("unable to read file");
+            }
+            students = JSON.parse(data);
+            resolve();
+        })
+    });
+};
+
+module.exports.getBSD = function(){
+    return new Promise((resolve, reject) => {
+        if(students.length == 0){
+            reject("no results returned");
+        }
+        
+        resolve(students);
+    
+    })
+}
+
+module.exports.highGPA = function(){
+    return new Promise((resolve, reject) => {
+        var Found = false;
+        var std_highGPA = [];
+        for(var i = 0; i < students.length; i++){
+            if(students[i].gpa >= 4.0){
+                for(var j = 0; j < students.length; j++){
+                    if(students[i].name == students[j].name){
+                        continue;
+                    }
+                    else if(students[i].name != students[j].name && students[i].gpa == students[j].gpa){
+                        std_highGPA.push(students[i]);
+                        Found = true;
+                    }
+                    else{
+                        std_highGPA.push(students[i]);
+                        Found = true;
+                    }
+                }
+            }
+        }
+
+        if(Found == false){
+            reject( "Failed finding the student with the highest GPA");
+        }
+        
+        resolve(students);
+        
+    })
+}
